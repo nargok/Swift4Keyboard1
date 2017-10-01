100b7eb7d7332893720f218f32906c02f9a030cd
後閑諒一 <nargok@gmail.com>
2017-10-01 16:47:31 +0900

    Initial commit

--- Swift4Keyboard1/ViewController.swift
+++ Swift4Keyboard1/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var mailTextField: UITextField!
+    @IBOutlet weak var paswordTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mailTextField.delegate = self
+        paswordTextField.delegate = self
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        mailTextField.resignFirstResponder()
+        paswordTextField.resignFirstResponder()
+        
+    }
+    
+    @IBAction func signIn(_ sender: Any) {
+        
+        // mailとパスワードの値をつなげて表示する
+        resultLabel.text = mailTextField.text! + "" + paswordTextField.text!
+        
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // キーボードを閉じる
+        textField.resignFirstResponder()
+        
+        return true
+    }
+ 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
